00bc68e9b075ab2713fc030e9af1a3acbc716dbc
Josh <73870067+joshboyye@users.noreply.github.com>
2024-02-03 04:32:07 -0500

    lab2c using array to store

--- lab2/edit-array.cpp
+++ lab2/edit-array.cpp
@@ -2,7 +2,7 @@
 Author: Joshua Wang
 Course: CSCI-135
 Instructor: Tong Yi
-Assignment: Input validation, Lab 2B
+Assignment: Using arrays to store, update, and retrieve numbers.
 
 This program will ask a user to enter two int and it will print the first int to the last int that was entered  
 */
@@ -14,9 +14,34 @@ int main(){
     int myData[10];
 
     for(int i =0; i<10; i++){
-        myData[i];
+        myData[i] =1;
+        cout<<myData[i]<<" ";
     }
-    cout<<myData<<endl;
+    cout<<endl;
+
+    
+    int x,y;
+    do{
+        cout<<"Enter the index:";
+        cin>>x;
+
+        if(x>=0 && x<10 ){
+            cout<<"Enter the value: ";
+            cin>>y;
+
+            myData[x] =y;
+
+            for(int i=0; i<10; i++){
+                cout<<myData[i] << " ";
+            }
+            cout<<endl;
+
+        }else{
+            cout<<"out of range.\n";
+            break;
+        }
+        
+    } while(true);
 
     return 0;
 }
