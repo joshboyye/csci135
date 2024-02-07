2f1f3303d851057f11c95d7ffd6a4881e67cfe0e
joshboyye <joshuawwangg@gmail.com>
2024-02-07 05:04:20 -0500

    Change the code

--- homework/cost_per_milles.cpp
+++ homework/cost_per_milles.cpp
@@ -1,3 +1,12 @@
+/*
+Author: Joshua Wang
+Course: CSCI-136
+Instructor: Tong Yi
+Assignment: Homework E2.10 - 1 character per line
+
+This program will take a string and print each character 
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -13,19 +22,18 @@ int main(){
     cout<<"Enter the price of gas per gallon\n";
     cin>>cost;
 
-    costper100Miles=gas*100;
-    costperMile=tank/gas;
+    costper100Miles=(100 / gas) * cost;
+    costperMile=cost / gas;
     costperGallon = cost;
 
-    totalCost = gas*tank;
+    totalCost = tank * cost;
     
 
-    cout<<costper100Miles<<endl;
-    cout<<costperMile<<endl;
-    cout<<costperGallon<<endl;
-    cout<<"The dicstance the car can travel is: "<<totalCost<<" Miles "<<endl;
-
-
+    cout<<"Cost per 100 miles: "<<costper100Miles<<endl;
+    cout<<"Cost per mile: "<<costperMile<<endl;
+    cout<<"Cost per gallon: "<<costperGallon<<endl;
+    cout<<"The distance the car can travel is: "<<tank * gas<<" Miles "<<endl;
+    cout<<"The total cost to empty the tank is: "<<totalCost<<" Dollars "<<endl;
 
     return 0;
 }
