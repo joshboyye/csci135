2a8521a0b49c9ef46dc9865dde3e978a33798474
joshboyye <joshuawwangg@gmail.com>
2024-02-27 18:48:23 -0500

    homework

--- homework/e5.7.cpp
+++ homework/e5.7.cpp
@@ -1,12 +1,20 @@
+/*
+Author: Joshua Wang
+Course: CSCI-136
+Instructor: Tong Yi
+Assignment: E5.6 Write a function
+
+This program will take a string and a number and print the string n times
+*/
+
 #include <iostream>
 using namespace std;
 
-string repeat(string str, int n){
+void repeat(string str, int n){
     for(int i=0; i<n; i++){
         cout<<str;
     }
 }
-
 
 int main(){
     string str;
