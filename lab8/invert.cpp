cf281df8c8a7f2928a8fb15a98112f4534fcef1c
Josh <73870067+joshboyye@users.noreply.github.com>
2024-03-28 03:29:47 -0400

    Lab8A

--- lab8/invert.cpp
+++ lab8/invert.cpp
@@ -1,64 +1,95 @@
+/*
+Author: Joshua Wang
+Course: CSCI-135
+Instructor: Tong Yi
+  Description:
+    The program reads a PGM image from the file "inImage.pgm",
+    and outputs a modified image to "outImage.pgm"
+
+*/
 #include <iostream>
+#include <cassert>
+#include <cstdlib>
 #include <fstream>
+
 using namespace std;
 
 const int MAX_H = 512;
 const int MAX_W = 512;
 
+
 void readImage(int image[MAX_H][MAX_W], int &height, int &width) {
-    ifstream inFile("inImage.pgm", ios::binary);
+	char c;
+	int x;
+	ifstream instr;
+	instr.open("inImage.pgm");
 
-    string header;
-    inFile >> header;
-    inFile >> width >> height;
-    int max_val;
-    inFile >> max_val;
 
-    inFile.ignore();
+	instr >> c;
+	assert(c == 'P');
+	instr >> c;
+	assert(c == '2');
 
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j++) {
-            char pixel;
-            inFile.read(&pixel, 1);
-            image[i][j] = (unsigned char)pixel;
-        }
-    }
+	while ((instr>>ws).peek() == '#') {
+		instr.ignore(4096, '\n');
+	}
 
-    inFile.close();
+	instr >> width;
+	instr >> height;
+
+	assert(width <= MAX_W);
+	assert(height <= MAX_H);
+	int max;
+	instr >> max;
+	assert(max == 255);
+
+	for (int row = 0; row < height; row++)
+		for (int col = 0; col < width; col++)
+			instr >> image[row][col];
+	instr.close();
+	return;
 }
+
+
 
 void writeImage(int image[MAX_H][MAX_W], int height, int width) {
-    ofstream outFile("outImage.pgm", ios::binary);
+	ofstream ostr;
+	ostr.open("outImage.pgm");
+	if (ostr.fail()) {
+		cout << "Unable to write file\n";
+		exit(1);
+	};
 
-    outFile << "P5\n";
-    outFile << width << " " << height << "\n";
-    outFile << 255 << "\n";
+	ostr << "P2" << endl;
+	ostr << width << ' ';
+	ostr << height << endl;
+	ostr << 255 << endl;
 
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j++) {
-            char pixel = (char)image[i][j];
-            outFile.write(&pixel, 1);
-        }
-    }
-
-    outFile.close();
-}
-
-void invertImage(int image[MAX_H][MAX_W], int height, int width) {
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j++) {
-            image[i][j] = 255 - image[i][j];
-        }
-    }
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			assert(image[row][col] < 256);
+			assert(image[row][col] >= 0);
+			ostr << image[row][col] << ' ';
+			ostr << endl;
+		}
+	}
+	ostr.close();
+	return;
 }
 
 int main() {
-    int img[MAX_H][MAX_W];
-    int height, width;
 
-    readImage(img, height, width);
-    invertImage(img, height, width);
-    writeImage(img, height, width);
+	int img[MAX_H][MAX_W];
+	int h, w;
 
-    return 0;
+	readImage(img, h, w); 
+	int out[MAX_H][MAX_W];
+
+	for(int row = 0; row < h; row++) {
+		for(int col = 0; col < w; col++) {
+			out[row][col] = 255 - img[row][col];
+		}
+	}
+	writeImage(out, h, w);
+
 }
