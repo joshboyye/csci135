3c9767ab2b7538b33c6e40e44826f7184732a563
Josh <73870067+joshboyye@users.noreply.github.com>
2024-04-05 07:01:36 -0400

    lab9E

--- lab9/3d-space.cpp
+++ lab9/3d-space.cpp
@@ -3,10 +3,11 @@
 Author: Joshua Wang
 Course: CSCI-135
 Instructor: Tong Yi
-Assignment: Lab9C
+Assignment: Lab9E
 
-This program will create a class Coord3D that stores the x, y, and z coordinates of a point in 3D space. It will also have functions that calculate the distance from the origin, compare two points to see which is farther from the origin, and move a point in 3D space.
+This program will create a 3D coordinate dynamically and delete it after it is no longer needed.
 */
+
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -37,12 +38,44 @@ void move(Coord3D *ppos, Coord3D *pvel, double dt) {
     ppos->y = ppos->y + pvel->y * dt;
     ppos->z = ppos->z + pvel->z * dt;
 }
+
+// Lab9 task E
+Coord3D* createCoord3D(double x, double y, double z) {
+    Coord3D *p = new Coord3D;
+    p->x = x;
+    p->y = y;
+    p->z = z;
+    return p;
+}
+
+void deleteCoord3D(Coord3D *p) {
+    delete p;
+}
 // Lab9 task C
-int main() {
+/*int main() {
     Coord3D pos = {0, 0, 100.0};
     Coord3D vel = {1, -5, 0.2};
 
     move(&pos, &vel, 2.0); // object pos gets changed
     cout << pos.x << " " << pos.y << " " << pos.z << endl;
     // prints: 2 -10 100.4
+}*/
+// Lab9 task E
+int main() {
+    double x, y, z;
+    cout << "Enter position: ";
+    cin >> x >> y >> z;
+    Coord3D *ppos = createCoord3D(x,y,z);
+    
+    cout << "Enter velocity: ";
+    cin >> x >> y >> z;
+    Coord3D *pvel = createCoord3D(x,y,z);
+
+    move(ppos, pvel, 10.0);
+
+    cout << "Coordinates after 10 seconds: " 
+         << (*ppos).x << " " << (*ppos).y << " " << (*ppos).z << endl;
+
+    deleteCoord3D(ppos); // release memory
+    deleteCoord3D(pvel);
 }
