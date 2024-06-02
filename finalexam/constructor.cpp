072f9c6e410b9a01916e2253cdf450fd6879af8b
Josh <73870067+joshboyye@users.noreply.github.com>
2024-06-02 17:27:07 -0400

    Create constructor.cpp

---
+++ finalexam/constructor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+class Cars{
+public: 
+    string modelname;
+    int modelnumber;
+    list<string> CarInvtory;
+};
+
