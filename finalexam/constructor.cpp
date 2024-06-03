b660a210d776f07e9aba1672fb9acac13915c361
Josh <73870067+joshboyye@users.noreply.github.com>
2024-06-02 21:28:52 -0400

    update

--- finalexam/constructor.cpp
+++ finalexam/constructor.cpp
@@ -9,5 +9,26 @@ public:
     string modelname;
     int modelnumber;
     list<string> CarInvtory;
+
+    Cars(string name,int model){
+        modelname=name;
+        modelnumber=model;
+    }
+    void getInfo(){
+        cout<< "name of the car is " << modelname << endl;
+        cout << "ths is the model number " << modelnumber <<endl;
+        for(auto i : CarInvtory){
+            cout << i <<endl;
+        }
+    }
 };
 
+int main(){
+    Cars obj1("BMW",3);
+    obj1.CarInvtory.push_back("M4");
+    obj1.CarInvtory.push_back("i8");    
+
+    obj1.getInfo();
+
+    return 0;
+}
